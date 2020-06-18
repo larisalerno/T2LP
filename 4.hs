062a9e3dfc7da49df799dad1c3874562d61d0e52
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-06-17 21:53:36 -0300

    resolvido a 4

--- 4.hs
+++ 4.hs
@@ -4,8 +4,19 @@
 -- ׀Conversor de Numero Octal em Decimal    ׀
 -- └────────────────────────────────────────┘
 
+getString :: Char -> String
+getString c = [c]
+
+getInt :: String -> Int
+getInt xs = read xs :: Int
+
+octoCalc :: Int -> Int -> Int
+octoCalc i value = value*(8^(i-1))
 
 octalToDec :: String -> Int
-octalToDec string = getInt (string !! 2)
+octalToDec [] = 0
+octalToDec xs = octoCalc (length xs) (getInt(getString (head xs))) + octalToDec (tail xs) 
 
-main = print(octalToDec "101")
+testeGetString = print(getString '1')
+testeGetInt = print(getInt "1")
+testeOctalToDec = print(octalToDec "23")
