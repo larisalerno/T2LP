8902345acfea42b6a1204ea06dafa2a0f3581536
Larissa Salerno <larissa.salerno@acad.pucrs.br>
2020-06-17 22:08:45 -0300

    arrumando a escrita do myMap

--- 3.hs
+++ 3.hs
@@ -13,7 +13,7 @@ luhnDouble :: [Int] -> [Int]
 luhnDouble [] = []
 luhnDouble xs =  if (head xs)*2 < 9 then (head xs)*2:luhnDouble (tail xs) else ((head xs)*2)-9: luhnDouble (tail xs)
 
--- main = print(luhnDouble [1,3,5,9,10])
+main = print(luhnDouble [1,3,5,9,10])
 
 luhn :: [Int] -> Bool
 luhn xs = (sum (luhnDouble xs)) `mod` 10 == 0
