512b0ec30b3deb5c62a876c502b2e0df85832ce6
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-06-18 15:13:02 -0300

    teste 4 resolvido

--- 4.hs
+++ 4.hs
@@ -7,17 +7,18 @@
 getString :: Char -> String
 getString c = [c]
 
-testeValue :: String -> Bool
-testeValue n = elem n ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","Y","W","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","w","z"] 
+testeValue :: [Char] -> Bool
+testeValue [] = False
+testeValue xs = if elem (head xs) ['a'..'z'] || elem (head xs) ['A'..'Z'] then True || testeValue (tail xs) else False || testeValue (tail xs)   
 
 getInt :: String -> Int
-getInt s = if (testeValue s) == False then read s :: Int else -1
+getInt s = read s :: Int
 
 octoCalc :: Int -> Int -> Int
 octoCalc i value = value*(8^(i-1))
 
 octalToDec :: String -> Int
 octalToDec [] = 0
-octalToDec xs = if (getInt(getString(head xs))) == -1 then octalToDec (drop (length xs) xs ) else octoCalc (length xs) (getInt(getString (head xs))) + octalToDec (tail xs) 
+octalToDec xs = if (testeValue xs) == True then 0 else octoCalc (length xs) (getInt(getString (head xs))) + octalToDec (tail xs) 
 
 testeOctalToDec = print(octalToDec "23")
