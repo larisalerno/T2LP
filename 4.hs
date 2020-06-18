acbe05fce6b526691e8e9554bb1e76dcf2994b12
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-06-17 22:56:53 -0300

    4 arrumada

--- 4.hs
+++ 4.hs
@@ -7,16 +7,17 @@
 getString :: Char -> String
 getString c = [c]
 
+testeValue :: String -> Bool
+testeValue n = elem n ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","Y","W","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","w","z"] 
+
 getInt :: String -> Int
-getInt xs = read xs :: Int
+getInt s = if (testeValue s) == False then read s :: Int else -1
 
 octoCalc :: Int -> Int -> Int
 octoCalc i value = value*(8^(i-1))
 
 octalToDec :: String -> Int
 octalToDec [] = 0
-octalToDec xs = octoCalc (length xs) (getInt(getString (head xs))) + octalToDec (tail xs) 
+octalToDec xs = if (getInt(getString (head xs))) == -1 then 0 else octoCalc (length xs) (getInt(getString (head xs))) + octalToDec (tail xs) 
 
-testeGetString = print(getString '1')
-testeGetInt = print(getInt "1")
 testeOctalToDec = print(octalToDec "23")
